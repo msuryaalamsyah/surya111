ca10337b6d5338cc44c30605a0697f7fe841acb2
msuryaalamsyah <nangisan008@gmail.com>
2025-01-16 10:19:37 +0700

    fix : text

---
+++ latihan_php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   
+    define('gaji',5000000);
+    define('bonus',500000);
+    define('pajak',0.12)
+    $proyek = 2;
+
+    //menghitung gaji+bonus
+    $proyek *= bonus; // = $proyek = $proyek = $bonus
+
+    //menghitung total pajak
+    $total_pajak = gaji + $proyek;
+
+    //gaji bersih
+    $gaji_bersih = $total_gaji - $total_pajakk;
+
+
+
+
+
+
+
+    echo "ini bonus kamu $proyek <br/>";
+    echo "ini gaji kamu sehabis ditambah bonus $total_gaji <br/>";
+    echo "ini pajak kamu $total_pajak <br/>";
+    echo "ini gaji bersih kamu $gaji_bersih"
+    
+   ?>
+</body>
+</html>
